cef859c351466932128fdaf12eb3d8dbd6fa5a8e
PDaguis <pedro.daguis13@gmail.com>
2025-02-08 18:13:56 -0300

    feat: adicionado alguns testes

---
+++ Hackaton.Test/User/UsuarioTests.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using Hackaton.API.DTO.Inputs.Usuario;
+using Hackaton.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton.Test.Usuario
+{
+    public class UsuarioTests
+    {
+        private readonly Faker _faker;
+
+        public UsuarioTests()
+        {
+            _faker = new Faker("pt_BR");
+        }
+
+        [Fact]
+        public void Should_Register_Infos_Not_Be_Null()
+        {
+            // Arrange
+            var usuario = new CadastrarInput();
+
+            // Act
+            usuario.Nome = _faker.Person.FirstName;
+            usuario.Email = _faker.Person.Email;
+            usuario.Senha = string.Empty;
+
+            // Assert
+            Assert.NotNull(usuario.Senha);
+        }
+    }
+}
